e089736ab3d5bf58dcd26704471465c1d5cf1a94
lukesh <adelukesh@gmail.com>
2025-04-22 18:25:40 +0530

    adding test case forgot password 004

--- testCases/test_forgot_password.py
+++ testCases/test_forgot_password.py
@@ -92,6 +92,38 @@ class Test_004_Forgot_Password:
 
     @pytest.mark.sanity
     def test_forgot_password_004(self, setup):
-        pass
+        self.driver = setup
+        self.logger.info("***************** Test Forgot Password 004 Is Start *****************")
+        try:
+            self.driver.get(self.baseURL)
+            self.lp = LoginPage(self.driver)
+            self.lp.click_on_my_account()
+            self.logger.info("clicking on my account")
+            self.lp.click_on_login_link()
+            self.logger.info("clicking on login link")
+            self.fp = ForgotPasswordPage(self.driver)
+            self.fp.clicking_on_forgot_password_link()
+            self.logger.info("clicking on forgot password link")
+            self.fp.clicking_on_continue_button()
+            self.logger.info("Clicking on continue button")
+            self.logger.info("************* Verifying Test Forgot Password 004 **************")
+            self.error_message = self.driver.find_element(By.XPATH, "//p[contains(text(),'Enter the e-mail address associated with your acco')]")
+            if "Enter the e-mail address associated with your account" in self.error_message.text:
+                assert True
+                self.logger.info("*********** Test Forgot Password 004 Is Passed *********")
+            else:
+                print("No error message displayed for the empty 'E-Mail Address' field.")
+                self.logger.info("*********** Test Forgot Password 004 Is Failed ************")
+                assert False
+        finally:
+                self.driver.quit()
+
+
+
+
+
+
+
+
 
 
