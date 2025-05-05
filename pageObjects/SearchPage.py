d1b3a85829261830a6b567bd7e99568c56ffd4d7
lukesh <adelukesh@gmail.com>
2025-05-05 16:35:23 +0530

    adding test product compare 015

--- pageObjects/SearchPage.py
+++ pageObjects/SearchPage.py
@@ -3,13 +3,14 @@ from selenium.webdriver.common.by import By
 
 class SearchPage:
 
-    textBox_searchField = (By.XPATH, "//input[@name='search' and @type='text']")
+    textBox_searchField = (By.NAME, "search")
     button_search = (By.XPATH, "//button[@class='btn btn-default btn-lg' and @type='button']")
 
     def __init__(self, driver):
         self.driver = driver
 
     def search_product(self, product):
+        self.driver.find_element(*SearchPage.textBox_searchField).clear()
         self.driver.find_element(*SearchPage.textBox_searchField).send_keys(product)
 
     def click_on_search_button(self):
