f5e0332ed4ac5414626e1c2daf8e72484b41d521
lukesh <adelukesh@gmail.com>
2025-07-04 14:05:40 +0530

    adding test shopping cart 002

--- pageObjects/ShoppingCartPage.py
+++ pageObjects/ShoppingCartPage.py
@@ -1,4 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions  as EC
 
 
 class ShoppingCartPage:
-    pass
+    shoppingCartHeaderOption = (By.XPATH, "//span[normalize-space()='Shopping Cart']")
+
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_on_shopping_cart_header_option(self):
+        shopping_cart_header_option = WebDriverWait(self.driver, 10, poll_frequency=2).until(
+            EC.element_to_be_clickable(ShoppingCartPage.shoppingCartHeaderOption)
+        )
+        shopping_cart_header_option.click()
--- testCases/test_shopping_cart.py
+++ testCases/test_shopping_cart.py
@@ -4,6 +4,7 @@ from utilities.readProperties import ReadConfig
 from pageObjects.SearchPage import SearchPage
 from pageObjects.ProductComparePage import ProductComparePage
 from pageObjects.ProductDisplayPage import ProductDisplayPage
+from pageObjects.ShoppingCartPage import ShoppingCartPage
 
 
 @pytest.mark.usefixtures("setup", "log_on_failure")
@@ -38,6 +39,36 @@ class Test_010_Shopping_Cart:
             self.logger.error("************** Test Shopping Cart 001 is Passed **************")
             assert False
         self.logger.info("*************************** End Of Test Shopping Cart 001 ***************************")
+
+    @pytest.mark.sanity
+    def test_shopping_cart_002(self, setup):
+        self.logger.info("*************************** Test Shopping Cart 002 is Start ****************************")
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.logger.info("Navigating to the base url")
+        self.sf = SearchPage(self.driver)
+        self.sf.search_product("iMac")
+        self.logger.info("Entering iMac to the search text box field")
+        self.sf.click_on_search_button()
+        self.logger.info("Clicking on the search icon button")
+        self.pc = ProductComparePage(self.driver)
+        self.pc.click_on_the_product_display_in_search_result()
+        self.logger.info("Clicking on the product display in search result")
+        self.sc = ShoppingCartPage(self.driver)
+        self.sc.click_on_shopping_cart_header_option()
+        self.logger.info("Clicking on the shopping cart header option")
+        self.logger.info("*************************** Verifying Test Shopping Cart 002 ****************************")
+        if self.driver.title == "Shopping Cart":
+            assert True
+            self.logger.info("****************** Test Shopping Cart 002 is Passed *****************")
+        else:
+            self.logger.error(f"Actual Title is: {self.driver.title}")
+            self.logger.error("****************** Test Shopping Cart 002 is Passed *****************")
+            assert False
+        self.logger.info("*************************** End Of Test Shopping Cart 002 ****************************")
+
+
+
 
 
 
